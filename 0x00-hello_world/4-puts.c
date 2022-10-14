5760fc1332bbd538d2c32e4a5f73e78dc64be186
Phumzi1e <Phumzilemiec@gmail.com>
2022-10-14 09:00:53 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
- * main-printing
  *
- * Return: Always 0(succsessful)
- */
-in main(void)
- {
-	 puts("\"Programming is like building a multilingual
-      puzzle");
-	 return(0);
+ *  * main - Entry
+ *
+ *   * Return: Always 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+	        puts("\"Programming is like building a multilingual puzzle");
+
+		        return (0);
+
 }
