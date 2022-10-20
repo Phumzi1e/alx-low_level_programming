6f06e246934d40649cf1cc862250e5a2a431c92b
Phumzi1e <Phumzilemiec@gmail.com>
2022-10-20 16:29:19 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
  * print_line - Draws a str line
  * @n: Takes in an integer
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('_');
 		_putchar('\n');
 	}
 	else
 	{
 		int i;
 
-		for (i = 0; i <= n; i++)
 		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
